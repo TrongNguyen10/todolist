9bbefc193ef60ec8212abb2fcbba468aa2bd2315
TrongNguyen10 <trungtrung1220@gmail.com>
2025-04-09 19:52:28 +0700

    add addtodo button

--- component/Header.js
+++ component/Header.js
@@ -2,6 +2,9 @@ import html from "../core.js";
 
 
 function Header() {
+    window.addTodo = () => {
+        dispatch('add', document.querySelector('.new-todo').value.trim())
+    }
     return html`
         <header class="header">
             <h1>todos</h1>
@@ -10,7 +13,8 @@ function Header() {
                 placeholder="What needs to be done?" 
                 autofocus
                 onkeyup="event.keyCode == 13 && dispatch('add', this.value.trim())"
-            >
+            />
+            <i class="fa-solid fa-circle-plus" onclick="addTodo()"></i>
         </header>
     `
 }
